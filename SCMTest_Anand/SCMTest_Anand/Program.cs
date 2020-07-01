cfdac82ccdd7befff58b3bd36f6a6c4b448d2b70
Anand Prakash Tiwari <aptiwari11@gmail.com>
2020-07-01 15:37:00 +0530

    Created New Console app
    This is .net core app for SCM Test.

---
+++ SCMTest_Anand/SCMTest_Anand/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SCMTest_Anand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
